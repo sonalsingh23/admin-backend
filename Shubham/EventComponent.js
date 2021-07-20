c71ad56406bb8d8045fb8ea514e8db784fb637d3
Shubham0163 <54832169+Shubham0163@users.noreply.github.com>
2021-07-20 12:45:36 +0530

    responsive
    event component is not working properly.

---
+++ Shubham/EventComponent.js
@@ -0,0 +1,40 @@
+import React from 'react';
+function EventComponent() {
+  return (
+    <div >
+     <div className="event">
+     <div className="ename">
+      <p className="pevent">EVENT</p>
+      <div className="ediv">
+      <input type="date" className="dt"></input> 
+      <p  className="pcss">wedding</p>
+      </div>
+      <div className="ediv2">
+      <input type="date" className="dt"></input>
+      <p  className="pcss">wedding</p>
+      </div>
+      
+      <div className="ediv3" >
+      <input type="date" className="dt"></input> 
+      <p  className="pcss">wedding</p>
+      </div>
+      <div className="ediv4">
+      <input type="date" className="dt"></input>
+      <p  className="pcss">wedding</p>
+      </div>
+      <div className="ediv5">
+      <input type="date" className="dt"></input>
+      <p  className="pcss" >wedding</p>
+      </div>
+      <div className="ediv6">
+      <input type="date" className="dt"></input>
+      <p className="pcss">wedding</p>
+      </div>
+     </div>
+     </div>
+    
+    </div>
+  );
+}
+
+export default EventComponent;
---
+++ Shubham/SearchComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+function SearchComponent() {
+  return (
+    <div className="search" >
+     
+     <input type="text" placeholder="Search Product or brand here" name="search" className="scmt"></input>
+    <div className="nbtn">  <button type="submit">Hey</button></div>
+    </div>
+  );
+}
+
+export default SearchComponent;
