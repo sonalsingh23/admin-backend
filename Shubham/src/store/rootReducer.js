1a03d527808956d67997f910b28d0fc20684a4f8
Kamal Sharma <kamal.sharma.hb@gmail.com>
2024-12-05 11:29:29 +0530

    remove old code

--- Shubham/src/store/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import weatherReducer from './weatherSlice';
-
-export default combineReducers({
-  /**Add more reducers here*/
-  weather: weatherReducer,
-});
